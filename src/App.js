88a34b2796876271df5824db0c5cbfc535b7ba49
shubhampronteff <67738104+shubhampronteff@users.noreply.github.com>
2020-07-03 19:16:32 +0530

    03/07/2020
    practice of elements and components

--- src/App.js
+++ src/App.js
@@ -1,27 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Sample from './Views/Sample';
-import Sam from './Views/Sample1';
+import React from "react";
+import ReactDom from "react-dom";
+import New from "./New";
 
-function App() {
+import "./App.css";
+
+function App(employee) {
   return (
     <div className="App">
-      <Sample></Sample>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-         <Sam></Sam>
-        </a>
-      </header>
+      <h1>Welcome to Pronteff IT</h1>
+      <New name="uday" role="software"></New>
     </div>
   );
 }
---
+++ src/New.js
@@ -0,0 +1,7 @@
+import React from "react-dom";
+import ReactDom from "react-dom";
+
+function New() {
+  return <h1>hello world</h1>;
+}
+export default New;
